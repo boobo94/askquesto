239fbcd442bea2d895cadfa798abcf4c4f8588bf
boobo94 <militaru.bogdanalexandru@gmail.com>
2018-11-26 01:04:30 +0200

    Restructure folders.

--- server/lib/es/docker/run.sh
+++ server/lib/es/docker/run.sh
@@ -1,5 +1,5 @@
 
 # Docs
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html
-echo "Start elasic search..."
+echo "Start elastic search..."
 docker run -d --name="elasticsearch" -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.5.1
